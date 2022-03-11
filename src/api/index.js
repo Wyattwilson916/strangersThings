3d01ac6d223ddbc27219fc4d51050c58e11364aa
Wyatt Wilson <wyattwilson916@gmail.com>
2022-03-11 15:23:01 -0500

    Second Commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,19 @@
 export const registerUser = async (name, password) => {
-    const response = await fetch(/* API URL */)
+  const response = await fetch(
+    "https://strangers-things.herokuapp.com/api/2202-FTB-ET-WEB-FT/users/register",
     {
-    method:"POST"
-    headers:{
-        'content-type':'application/json'
-    },
-    body: JSON.stringify({
-        user:{
-            username,
-            password
-        }
-    })
-}
-)
-const data = await response.json()
-return data
-}
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          password
+        },
+      }),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
